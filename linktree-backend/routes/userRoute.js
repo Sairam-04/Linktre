d1e040584df200ade022a80398dae30ea9b25319
Sairam <sairamramavath01@gmail.com>
2024-03-01 12:14:07 +0530

    add: send forgot password link

--- linktree-backend/routes/userRoute.js
+++ linktree-backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controller/userController");
+const { registerUser, loginUser, sendForgotPasswordEmail } = require("../controller/userController");
 const userRouter = express.Router();
 
 userRouter.route("/register").post(registerUser);
 userRouter.route("/login").post(loginUser);
+userRouter.route("/send-password-reset-email").post(sendForgotPasswordEmail);
 
 module.exports = userRouter;
