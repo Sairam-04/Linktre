2a05c105e625c5c14e55a9ab1fc968ec9412ad20
Sairam <sairamramavath01@gmail.com>
2024-03-17 19:53:59 +0530

    Update: Create Link API Call

--- linktree-backend/controller/linksController.js
+++ linktree-backend/controller/linksController.js
@@ -4,41 +4,33 @@ const ErrorHandler = require("../utils/ErrorHandler");
 
 exports.createLink = async (req, res, next) =>{
     try {
-        req.body.user = req.user._id;
-        const existingData = await LinksData.findOne({user: req.user._id});
-        if(existingData){
-            return next(new ErrorHandler(400, "User has an Existing Social Links"));
-        } 
-        const link = await LinksData.create(req.body);
-        return res.status(200).json({
-            success: true,
-            message: `${req.body.linkTitle || "Link"} Successfully added`
-        }) 
-    } catch (error) {
-        return next(new ErrorHandler(400, "Unable to create a Link"));
-    }
-}
-
-exports.addLink = async (req, res, next) =>{
-    try {
         const user_id = req.user._id;
         if(!user_id){
             return next(new ErrorHandler(400, "User Not Found"));
-        }
-        const links = await LinksData.findOne({user: user_id});
-        if(!links){
-            return next(new ErrorHandler(400, "User Not Registered"))
         }
         const newLinksData = req.body;
         if(!newLinksData){
             return next(new ErrorHandler(400, "Data is not Valid!"));
         }
-        await links.socials.push(newLinksData);
-        links.save();
+        const existingData = await LinksData.findOne({user: req.user._id});
+        if(existingData){
+            existingData.socials.push(newLinksData);
+            await existingData.save();
+            return res.status(200).json({
+                success: true,
+                message: `${newLinksData.linkTitle || "Link"} Successfully added`
+            }) 
+        } 
+        const link = await LinksData.create({
+            user: user_id,
+            socials: [
+                newLinksData
+            ]
+        });
         return res.status(200).json({
             success: true,
-            message: `${newLinksData.linkTitle || "Link"} Successfully added`,
-        })
+            message: `${newLinksData.linkTitle || "Link"} Successfully added`
+        }) 
     } catch (error) {
         return next(new ErrorHandler(400, "Unable to create a Link"));
     }
--- linktree-backend/routes/linksRoute.js
+++ linktree-backend/routes/linksRoute.js
@@ -4,7 +4,6 @@ const linksRouter = express.Router();
 const AuthMiddleware = require("../middleware/AuthMiddleware");
 
 linksRouter.route("/create-link").post(AuthMiddleware, createLink);
-linksRouter.route("/add-link").post(AuthMiddleware, addLink);
 linksRouter.route("/edit-link/:linkId").put(AuthMiddleware, editLink);
 linksRouter.route("/delete-link/:linkId").delete(AuthMiddleware,deleteLink);
 linksRouter.route("/get-all-links/:username").get(getAllLinks);
